8647e105f68609ba675e487f81c0a22de09fab2e
Paul Isaris <paulisaris@gmail.com>
2019-04-27 11:45:06 +0300

    Added dialog data interface

--- src/app/services/dialog.service.ts
+++ src/app/services/dialog.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 
+export class InfoDialogData {
+  title: string;
+  message: string;
+  showOKBtn = false;
+  showCancelBtn = false;
+}
+
 @Injectable({
   providedIn: 'root'
 })
