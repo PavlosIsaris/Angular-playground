52cd2f56d6a52811e60a82bb28b18d07f8874535
Paul Isaris <paulisaris@gmail.com>
2019-04-27 13:00:27 +0300

    Creating and showing dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {DialogService} from './services/dialog.service';
+import {DialogData} from './shared/dialog-data';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,27 @@ import {DialogService} from './services/dialog.service';
 export class AppComponent {
   title = 'angular-playground';
 
-  constructor(private dialogService: DialogService) {}
+  constructor(private dialogService: DialogService) {
+  }
 
   openDialog() {
 
+    const dialogData: DialogData = {
+      title: 'Test Dialog',
+      message: 'This is our first dialog!',
+      showOKBtn: true,
+      showCancelBtn: true
+    };
+
+    const dialogRef = this.dialogService.openDialog(
+      dialogData, {disableClose: true});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('User clicked OK');
+      } else {
+        console.log('User clicked Cancel');
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { DialogComponent } from './dialog/dialog.component';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatDialogModule} from '@angular/material';
+import {DialogService} from './services/dialog.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogComponent
   ],
+  entryComponents: [
+    DialogComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [DialogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
